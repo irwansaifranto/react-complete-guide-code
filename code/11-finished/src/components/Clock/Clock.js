eced6754fac85e27f0980b531e9f51f0c4c2de0c
Irwan Saifranto R <irwansaifranto93@gmail.com>
2021-11-10 16:43:37 +0700

    State and LifeCycle

---
+++ code/11-finished/src/components/Clock/Clock.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.text = "Hello, world!";
+    this.state = { date: new Date() }; // The only place where you can assign this.state is the constructor.
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+    console.log(ReactDOM.findDOMNode(this));
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Unmount.");
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.text}</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default Clock;
